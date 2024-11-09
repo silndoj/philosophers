18d81172b98e7b699bd528b7e9b916642b34d5fb
Silvestri Ndoj <tarazhsilvi@gmail.com>
2024-11-09 03:00:39 +0100

    Created a Header file with needed Libraries

---
+++ philo/include/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: silndoj <silndoj@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 01:45:36 by silndoj           #+#    #+#             */
+/*   Updated: 2024/11/09 02:57:27 by silndoj          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <unistd.h>
+# include <sys/time.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <pthread.h>
+# include "garbage_collector.h"
+
+#endif

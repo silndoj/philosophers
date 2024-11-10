9c5e5ff3657805167dfa6464b66b75c247f4e11f
Silvestri Ndoj <silvesterndoj13@gmail.com>
2024-11-10 20:04:34 +0100

    created a struct and a function

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: silndoj <silndoj@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 01:45:36 by silndoj           #+#    #+#             */
-/*   Updated: 2024/11/09 02:57:27 by silndoj          ###   ########.fr       */
+/*   Updated: 2024/11/10 19:46:56 by silndoj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,16 @@
 # include <string.h>
 # include <pthread.h>
 # include "garbage_collector.h"
+
+typedef struct	philo_t
+{
+	pthread_t	*philos;
+	int			philo_idx;
+
+}	t_philo;
+
+//Initialize
+void			sit_on_table(char **argv, t_philo *philos);
+int				ft_atoi(const char *str);
 
 #endif

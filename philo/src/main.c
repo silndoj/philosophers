e1e49a9ee59ececc2c6b282af8f9305a3ec7bc7a
Silvestri Ndoj <silvesterndoj13@gmail.com>
2024-11-10 20:05:10 +0100

    simple test

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,8 +6,24 @@
 /*   By: silndoj <silndoj@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 02:40:12 by silndoj           #+#    #+#             */
-/*   Updated: 2024/11/09 02:57:25 by silndoj          ###   ########.fr       */
+/*   Updated: 2024/11/10 20:02:49 by silndoj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	t_philo	*philos;
+
+	if (argc == 2)
+	{
+		
+		philos = ft_malloc(sizeof(t_philo));
+		sit_on_table(argv, philos);
+		printf("%d\n", philos->philo_idx);
+	}
+	free_allocations();
+	return (0);
+}

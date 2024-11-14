8fabb9a48f100dfc938cc57aac2a59b9a8549fff
Silvestri Ndoj <silvesterndoj13@gmail.com>
2024-11-14 20:27:58 +0100

    edited the checker in main for arguments and only freeing if the init_philos(argv) will return (NULL)

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: silndoj <silndoj@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 02:40:12 by silndoj           #+#    #+#             */
-/*   Updated: 2024/11/13 17:14:32 by silndoj          ###   ########.fr       */
+/*   Updated: 2024/11/14 20:15:25 by silndoj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@ int	main(int argc, char *argv[])
 	t_philo	*philos;
 	int		i;
 
-	if (argc == 5)
+	if (argc == 5 || argc == 6)
 	{
 		philos = init_philos(argv);
+		if (!philos)
+		{
+			free_allocations();
+			return (1);
+		}
 		printf("address of struct = %p\n", philos);
 		i = sit_on_table(philos);
 		printf("return = (%d)\n", i);

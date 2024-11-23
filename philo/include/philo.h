3980871a7f0f6c27beb83abe2e802cabb7b66928
Silvestri Ndoj <silvesterndoj13@gmail.com>
2024-11-23 16:06:13 +0100

    added another struct private for each philo

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: silndoj <silndoj@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 01:45:36 by silndoj           #+#    #+#             */
-/*   Updated: 2024/11/23 14:23:08 by silndoj          ###   ########.fr       */
+/*   Updated: 2024/11/23 16:03:34 by silndoj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,36 @@
 # include <pthread.h>
 # include "garbage_collector.h"
 
-typedef struct	philo_t
+typedef struct private_t
 {
+	int				idx;
+	int				meals_eaten;
+	int				number_of_meals;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*r_fork;
+	struct philo_t	*philos;
+}	t_private;
+
+typedef struct philo_t
+{
+	int				flag;
 	int				philo_idx;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				number_of_meals;
+	pthread_mutex_t	*forks;
+	t_private		*p_private;
 }	t_philo;
 
 //Initialize
-int				sit_on_table(t_philo *philos);
 int				init_philos(t_philo *philos, char **argv, int argc);
+int				philos_init(t_philo *philos, int argc, char **argv);
 
 //Utils
 int				is_digit(char c);
 int				ft_atoi(const char *str);
-int				philos_checker(t_philo *philos, int argc, char **argv);
 int				arg_checker(char **argv);
+int				philos_checker(t_philo *philos, int argc, char **argv);
 
 #endif

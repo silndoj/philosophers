c906805ef2aa3e5f54d816f9b0b51ca6f7ec5af7
Silvestri Ndoj <silvesterndoj13@gmail.com>
2024-11-27 19:13:26 +0100

    added the death cam which survey the death of one of the threads and put the flag to 0

--- philo/src/play_routine.c
+++ philo/src/play_routine.c
@@ -6,11 +6,36 @@
 /*   By: silndoj <silndoj@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 16:54:40 by silndoj           #+#    #+#             */
-/*   Updated: 2024/11/23 17:06:58 by silndoj          ###   ########.fr       */
+/*   Updated: 2024/11/27 19:00:39 by silndoj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
+
+void	death_cam(t_philo *philos)
+{
+	int	i;
+
+	i = 0;
+	while (philos->flag)
+	{
+		i = 0;
+		while (i < philos->philo_idx)
+		{
+			if ((actual_time() - philos->p_private[i].last_eat_time)
+				> philos->time_to_die)
+			{
+				philos->flag = 0;
+				pthread_mutex_lock(&philos->single_lock);
+				printf("%ld %d died\n", actual_time(), \
+				philos->p_private[i].idx);
+				pthread_mutex_unlock(&philos->single_lock);
+				break ;
+			}
+			i++;
+		}
+	}
+}
 
 void	play_routine(t_philo *philos)
 {
@@ -21,6 +46,7 @@ void	play_routine(t_philo *philos)
 	while (++i < philos->philo_idx)
 		pthread_create(&philos->threads[i], NULL, routine,
 			&philos->p_private[i]);
+	death_cam(philos);
 	i = -1;
 	while (++i < philos->philo_idx)
 		pthread_join(philos->threads[i], NULL);

e6aecfd100915259ef3e6a4cdaef1d4153ef2c84
Silvestri Ndoj <silvesterndoj13@gmail.com>
2024-12-04 16:24:41 +0100

    Update p_take_forks and p_eat functions in routine.c (part 1)

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -15,23 +15,42 @@
 
 void	p_take_forks(t_philo *philos, t_private *p_private)
 {
-	pthread_mutex_lock(p_private->l_fork);
+	pthread_mutex_t *first_fork;
+	pthread_mutex_t *second_fork;
+
+	if (p_private->idx % 2 == 0)
+	{
+		first_fork = p_private->r_fork;
+		second_fork = p_private->l_fork;
+	}
+	else
+	{
+		first_fork = p_private->l_fork;
+		second_fork = p_private->r_fork;
+	}
+	pthread_mutex_lock(first_fork);
 	pthread_mutex_lock(&philos->single_lock);
-	printf("%ld %d took left fork\n", actual_time(), p_private->idx);
+	printf("%ld %d has taken a fork\n", actual_time() - philos->start_time, p_private->idx);
 	pthread_mutex_unlock(&philos->single_lock);
-	pthread_mutex_lock(p_private->r_fork);
+	pthread_mutex_lock(second_fork);
 	pthread_mutex_lock(&philos->single_lock);
-	printf("%ld %d has took right fork\n", actual_time(), p_private->idx);
+	printf("%ld %d has taken a fork\n", actual_time() - philos->start_time, p_private->idx);
 	pthread_mutex_unlock(&philos->single_lock);
 }
 
 void	p_eat(t_philo *philos, t_private *p_private)
 {
-	p_private->last_eat_time = actual_time();
 	pthread_mutex_lock(&philos->single_lock);
-	printf("%ld %d is eating\n", actual_time(), p_private->idx);
+	p_private->last_eat_time = actual_time();
+	printf("%ld %d is eating\n", actual_time() - philos->start_time, p_private->idx);
 	pthread_mutex_unlock(&philos->single_lock);
+	
+	precise_usleep(philos->time_to_eat);
+	
+	pthread_mutex_lock(&philos->single_lock);
 	p_private->meals_eaten++;
+	pthread_mutex_unlock(&philos->single_lock);
+	
 	pthread_mutex_unlock(p_private->l_fork);
 	pthread_mutex_unlock(p_private->r_fork);
 }
